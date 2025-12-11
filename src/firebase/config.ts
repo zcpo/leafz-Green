71cc8180e6424e094f098af3979768532dab0a1e
Prop Theme <proptheme@gmail.com>
2025-12-11 08:33:52 +0000

    Build Logs 27s 23 lines selected (348ms) Find in logs ⌘F 03:22:47.161 R

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,5 +5,6 @@ export const firebaseConfig = {
   appId: "1:7204635795:web:b617d33e54b6a568853755",
   apiKey: "your_public_api_key_is_not_a_secret",
   authDomain: "studio-7204635795-e0826.firebaseapp.com",
-  messagingSenderId: "7204635795"
+  messagingSenderId: "7204635795",
+  storageBucket: "studio-7204635795-e0826.appspot.com",
 };
